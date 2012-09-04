66bce6c1d6d1f7038565b21512677ffbaffeb879
Glenn Fiedler <glenn.fiedler@gmail.com>
2012-09-03 23:19:38 -0700

    wip on exact calculation of biconvex inertia tensor

--- VirtualGo.cpp
+++ VirtualGo.cpp
@@ -29,6 +29,7 @@ using namespace vectorial;
     Biconvex solid.
 
     The biconvex solid is the intersection of two equally sized spheres
+
     The two spheres of specific radius are placed vertically relative to 
     each other at a specific distance to generate a biconvex solid with
     the desired width (circle diameter) and height (y axis top to bottom).
@@ -662,6 +663,9 @@ void CalculateEllipsoidInertiaTensor( float mass, float a, float b, float c, mat
     const float i_a = 1.0f/5.0f * mass * ( b*b + c*c );
     const float i_b = 1.0f/5.0f * mass * ( a*a + c*c );
     const float i_c = 1.0f/5.0f * mass * ( a*a + b*b );
+    printf( "ellipsoid ix = %f\n", i_a );
+    printf( "ellipsoid iy = %f\n", i_b );
+    printf( "ellipsoid iz = %f\n", i_c );
     float values[] = { i_a,   0,   0, 0, 
                          0, i_b,   0, 0,
                          0,   0, i_c, 0,
@@ -700,11 +704,11 @@ void CalculateBiconvexInertiaTensor( float mass, const Biconvex & biconvex, mat4
     const double v = CalculateBiconvexVolume( biconvex );
     const double p = mass / v;
     const double m = dx*dy*dz * p;
-    for ( int index_z = 0; index_z < xz_steps; ++index_z )
+    for ( int index_z = 0; index_z <= xz_steps; ++index_z )
     {
-        for ( int index_y = 0; index_y < y_steps; ++index_y )
+        for ( int index_y = 0; index_y <= y_steps; ++index_y )
         {
-            for ( int index_x = 0; index_x < xz_steps; ++index_x )
+            for ( int index_x = 0; index_x <= xz_steps; ++index_x )
             {
                 const double x = sx + index_x * dx;
                 const double y = sy + index_y * dy;
@@ -714,17 +718,41 @@ void CalculateBiconvexInertiaTensor( float mass, const Biconvex & biconvex, mat4
 
                 if ( PointInsideBiconvex_LocalSpace( point, biconvex ) )
                 {
-                    const double rx = sqrt( z*z + y*y );
-                    const double ry = sqrt( x*x + z*z );
-                    const double rz = sqrt( x*x + y*y );
+                    const double rx2 = z*z + y*y;
+                    const double ry2 = x*x + z*z;
+                    const double rz2 = x*x + y*y;
 
-                    ix += rx * m;
-                    iy += ry * m;
-                    iz += rz * m;
+                    ix += rx2 * m;
+                    iy += ry2 * m;
+                    iz += rz2 * m;
                 }
             }
         }
     }
+
+    printf( "p = %f\n", p );
+
+    printf( "approx ix = %f\n", ix );
+    printf( "approx iy = %f\n", iy );
+
+    {
+        // http://wolframalpha.com
+        // integrate ( r^2 - ( y + r - h/2 ) ^ 2 ) ^ 2 dy from y = 0 to h/2
+        //  => 1/480 h^3 (3 h^2-30 h r+80 r^2)
+        const float h = height;
+        const float r = biconvex.GetSphereRadius();
+        const float h2 = h * h;
+        const float h3 = h2 * h;
+        const float h4 = h3 * h;
+        const float h5 = h4 * h;
+        const float r2 = r * r;
+        const float r3 = r2 * r;
+        const float r4 = r3 * r;
+        const float exact_iy = pi * p * ( 1/480.0f * h3 * ( 3*h2 - 30*h*r + 80*r2 ) );
+
+        printf( "exact iy = %f\n", exact_iy );
+    }
+
     const float values[] = { ix,  0,  0, 0, 
                               0, iy,  0, 0, 
                               0,  0, iz, 0, 
@@ -1236,6 +1264,8 @@ inline void ClosestFeaturesStoneBoard( const Board & board,
     {
         Biconvex biconvex( 2.0f, 1.0f );
         RigidBody rigidBody;
+        printf( "calculating ellipsoid inertia tensor...\n" );
+        CalculateEllipsoidInertiaTensor( rigidBody.mass, 1,0.5f,1, rigidBody.inertiaTensor, rigidBody.inverseInertiaTensor );
         printf( "calculating biconvex inertia tensor...\n" );
         CalculateBiconvexInertiaTensor( rigidBody.mass, biconvex, rigidBody.inertiaTensor, rigidBody.inverseInertiaTensor );
         printf( "done." );
@@ -2464,7 +2494,7 @@ inline void ClosestFeaturesStoneBoard( const Board & board,
 
                                 // calculate normal impulse
 
-                                const float e = 0.45f;
+                                const float e = 0.4f;
 
                                 vec3f r = stonePoint - rigidBody.position;
 
